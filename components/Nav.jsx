e9546776dff1397b459a71fd4624a0ae185e95c6
Pakin Charoenkitchaikarn <knigh@Mr-KreeG3r.local>
2023-06-12 15:31:16 +0700

    Log In

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,21 @@ import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserloggedIn = true;
+
+  const [providers, setProvider] = useState
+  (null);
+
+  useEffect(() => {
+    const setProvider = async () => {
+      const response = await getProviders();
+
+      setProvider(response);
+    }
+
+    setProvider();
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -20,6 +35,45 @@ const Nav = () => {
         width={80}
         height={50}/>
       </Link>
+
+      {/* desktop navigation */}
+      <div className="sm:flex hidden">
+        {isUserloggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href="/create-prompt" className='black_btn'>
+              Create Post
+            </Link>
+
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href="/profile">
+              <Image src="/assets/images/LOGO2.png" 
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              />
+            </Link>
+          </div>
+        ): (
+          <>
+          {providers && 
+            Object.keys(providers).map((provider) =>
+            (
+              <button
+              type="button"
+              key={provider.name}
+              onClick={() => signIn(provider.id)}
+              className='black_btn'
+              >
+                Sign In
+              </button>
+            ))}
+          </>
+        )}
+      </div>
     </nav>
   )
 }
